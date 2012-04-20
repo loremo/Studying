e648ca5c4f472774618b2a7ab0d7e16df6a6c320
dimko <dimko@ubuntu.(none)>
2012-04-20 22:41:09 +0200

    x

--- Systemnahe Software/Uebung3/macros.h
+++
@@ -1,6 +0,0 @@
-#define I_DONT_KNOW I dont know
-#define HOW how you were
-#define I_LOOK I look at
-#define YOU_ALL_SEE you all see the love there that is sleeping
-
-#define GENTLY(_start) _start my guitar gently weeps
